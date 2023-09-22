85e1ad7de8497949d22cff659eda9c46bd33498a
Ginga Kennis <gingakennis123@icloud.com>
2023-09-22 17:31:55 +0900

    confirm tsdf-grasp candidates

--- scripts/ur5e_grasp.py
+++ scripts/ur5e_grasp.py
@@ -1,4 +1,6 @@
 #!/usr/bin/env python
+import argparse
+from pathlib import Path
 
 import cv_bridge
 import numpy as np
@@ -7,9 +9,12 @@ import sensor_msgs.msg
 import time
 
 from vgn import vis
+from vgn.experiments.clutter_removal import State
+from vgn.detection import VGN
 from vgn.utils import ros_utils
 from vgn.utils.transform import Rotation, Transform
 from vgn.perception import *
+from vgn.utils.ur5e_control import Ur5eCommander
 
 # tag lies on the table in the center of the workspace
 T_base_tag = Transform(Rotation.identity(), [0.1, 0.5, 0.1]) # identity transformation
@@ -17,10 +22,16 @@ T_base_tag = Transform(Rotation.identity(), [0.1, 0.5, 0.1]) # identity transfor
 class Ur5eGraspController(object):
     def __init__(self):
         self.size = rospy.get_param("/ur5e_grasp/size")
+        self.finger_depth = rospy.get_param("/ur5e_grasp/finger_depth")
         self.base_frame_id = rospy.get_param("/ur5e_grasp/base_frame_id")
+        self.scan_joints = rospy.get_param("/ur5e_grasp/scan_joints")
         self.tf_tree = ros_utils.TransformTree()
         self.define_workspace()
         self.tsdf_server = TSDFServer()
+        self.ur5e_commander = Ur5eCommander()
+        self.plan_grasps = VGN(args.model, rviz=True)
+
+    
 
     def define_workspace(self):
         z_offset = -0.06
@@ -36,17 +47,31 @@ class Ur5eGraspController(object):
         time.sleep(10.0)
         vis.clear()
         vis.draw_workspace(self.size)
-        # move to initial position
+        self.ur5e_commander.goto_home()
+
         tsdf, pc = self.acquire_tsdf()
         vis.draw_tsdf(tsdf.get_grid().squeeze(), tsdf.voxel_size)
         vis.draw_points(np.asarray(pc.points))
+        rospy.loginfo("Reconstructed scene")
+
+        state = State(tsdf, pc)
+        grasps, scores, planning_time = self.plan_grasps(state)
+        print(grasps)
+        vis.draw_grasps(grasps, scores, self.finger_depth)
+        rospy.loginfo("Planned grasps")
+
+        if len(grasps) == 0:
+            rospy.loginfo("No grasps detected")
+            return
     
     def acquire_tsdf(self):
+        self.ur5e_commander.goto_joints(self.scan_joints[0])
+
         self.tsdf_server.reset()
         self.tsdf_server.integrate = True
-        print("integrate tsdf")
-        # move to positions to get depth image
-        time.sleep(1.0)
+
+        for joint_target in self.scan_joints[1:]:
+            self.ur5e_commander.goto_joints(joint_target)
 
         self.tsdf_server.integrate = False
         tsdf = self.tsdf_server.low_res_tsdf
@@ -98,4 +123,7 @@ def main():
 
 
 if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--model", type=Path, required=True)
+    args = parser.parse_args()
     main()
---
+++ src/vgn/utils/ur5e_control.py
@@ -0,0 +1,44 @@
+import moveit_commander
+import rospy
+
+from vgn.utils import ros_utils
+
+
+class Ur5eCommander(object):
+    def __init__(self):
+        self.name = "ur5e"
+        self.connect_to_move_group()
+
+    def connect_to_move_group(self):
+        self.robot = moveit_commander.RobotCommander()
+        self.scene = moveit_commander.PlanningSceneInterface()
+        self.move_group = moveit_commander.MoveGroupCommander(self.name)
+
+    def goto_home(self):
+        self.goto_joints([1.5708,-1.5708,0,-1.5708,0,0])
+
+    def goto_joints(self,joints,velocity_scaling=0.1,acceleration_scaling=0.1):
+        self.move_group.set_max_velocity_scaling_factor(velocity_scaling)
+        self.move_group.set_max_acceleration_scaling_factor(acceleration_scaling)
+        self.move_group.set_joint_value_target(joints)
+        plan = self.move_group.plan()[1]
+        
+        user_input = input("EXECUTE PLAN [y/n] : ")
+        if user_input == "y":
+            success = self.move_group.execute(plan, wait=True)
+        else:
+            print("ABORTED PLAN")
+
+        self.move_group.stop()
+        return success
+    
+    def goto_pose(self,pose,velocity_scaling=0.1,acceleration_scaling=0.1):
+        pose_msg = ros_utils.to_pose_msg(pose)
+        self.move_group.set_max_velocity_scaling_factor(velocity_scaling)
+        self.move_group.set_max_acceleration_scaling_factor(acceleration_scaling)
+        self.move_group.set_pose_target(pose_msg)
+        plan = self.move_group.plan()[1]
+        success = self.move_group.execute(plan, wait=True)
+        self.move_group.stop()
+        self.move_group.clear_pose_targets()
+        return success
