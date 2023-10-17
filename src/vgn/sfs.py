78c7b085e901fc08f2872ea40f52b0d83f809ec6
Ginga Kennis <gingakennis123@icloud.com>
2023-10-17 21:50:34 +0900

    update sfs

--- src/vgn/sfs.py
+++ src/vgn/sfs.py
@@ -1,56 +1,96 @@
 import numpy as np
 from scipy.spatial.transform import Rotation as R
-import sys
 import cv2
 
 class VoxelSpace:
-    def __init__(self, x_lim, y_lim, z_lim, voxel_size):
+    def __init__(self, xlim, ylim, zlim, voxel_size,K):
         # number of voxel in each axis
-        self.voxel_number = [np.abs(x_lim[1] - x_lim[0]) / voxel_size,np.abs(y_lim[1] - y_lim[0]) / voxel_size, np.abs(z_lim[1] - z_lim[0]) / voxel_size]
+        self.voxels_number = [np.abs(xlim[1] - xlim[0]) / voxel_size[0], np.abs(ylim[1] - ylim[0]) / voxel_size[1], np.abs(zlim[1] - zlim[0]) / voxel_size[2]]
+        self.voxels_number_act = np.array(self.voxels_number).astype(int) + 1
         
         # total number of voxels
-        self.total_number = np.prod(self.voxel_number).astype(int)
+        self.total_number = np.prod(self.voxels_number_act).astype(int)
 
         # (total_number, 4)
         # The first three values are the x-y-z-coordinates of the voxel, the fourth value is the occupancy
         self.voxel = np.ones((self.total_number, 4))
 
-        self.voxel3Dx = np.meshgrid(np.linspace(x_lim[0], x_lim[1], self.voxel_number[0].astype(int)))
-        self.voxel3Dy = np.meshgrid(np.linspace(y_lim[0], y_lim[1], self.voxel_number[1].astype(int)))
-        self.voxel3Dz = np.meshgrid(np.linspace(z_lim[0], z_lim[1], self.voxel_number[2].astype(int)))
+        self.sx = xlim[0]
+        self.ex = xlim[1]
+        self.sy = ylim[0]
+        self.ey = ylim[1]
+        self.sz = zlim[0]
+        self.ez = zlim[1]
 
         l = 0
-        for z in np.linspace(z_lim[0], z_lim[1], self.voxel_number[2].astype(int)):
-            for x in np.linspace(x_lim[0], x_lim[1], self.voxel_number[0].astype(int)):
-                for y in np.linspace(y_lim[0], y_lim[1], self.voxel_number[1].astype(int)):
+        for z in np.linspace(self.sz, self.ez, self.voxels_number_act[2]):
+            for x in np.linspace(self.sx, self.ex, self.voxels_number_act[0]):
+                for y in np.linspace(self.sy, self.ey, self.voxels_number_act[1]):
                     self.voxel[l] = [x, y, z, 1] 
                     l += 1
 
+        self.object_points3D = np.copy(self.voxel).T
+        self.voxel[:,3] = 0
 
-                
+        # camera intrinsic
+        self.K = K
 
+    def sfs(self, image, extrinsic):
+        height, width, image, silhouette = self.preprocess_image(image)
+        print(height)
+        print(width)
+
+        #perspective projection matrix
+        p_matrix = self.calc_p_matrix(extrinsic)
+
+
+        # projection to the image plane
+        points2D = np.matmul(p_matrix, self.object_points3D)
+        points2D = np.floor(points2D / points2D[2, :]).astype(np.int32)
+        print(points2D)
+        print(np.where(points2D < 0))
+        points2D[np.where(points2D < 0)] = 0  # check for negative image coordinate
 
         
+        ind1 = np.where(points2D[0, :] >= width) # check for out-of-bounds (width) coordinate
+        points2D[:,ind1] = 0
+        ind2 = np.where(points2D[1, :] >= height)  # check for out-of-bounds (height) coordinate
+        points2D[:,ind2] = 0
 
+        # accumulate the value of each voxel in the current image
+        self.voxel[:,3] += silhouette.T[points2D.T[:,0], points2D.T[:,1]]
 
-    #     # camera properties
-    #     self.focal_length = focal_length
-    #     self.K = np.ndarray([self.fx, 0.0, self.cx], [0.0, self.fy, self.cy], [0.0, 0.0, 1.0])
+    def calc_p_matrix(self,extrinsic):
+        return np.matmul(self.K,extrinsic)
+    
+    def preprocess_image(self,image):
+        height, width = np.shape(image)
+        image[np.where(image != 0)] = 1
+        silhouette = image > 0
+        return height, width, image, silhouette
 
-    # def sfs(self, image, extrinsic):
-    #     height, width = np.shape(image)
-    #     p_matrix = self.calc_p_matrix(extrinsic)
-
-    #     image_plane = np.matmul(p_matrix,self.voxel)
-
-
-
-    # def calc_p_matrix(self,extrinsic):
-    #     return np.matmul(self.K,extrinsic)
+def preprocess_image(image):
+        height, width = np.shape(image)
+        image[np.where(image != 0)] = 1
+        silhouette = image > 0
+        return height, width, image, silhouette
 
 
 if __name__ == "__main__":
-    voxel = VoxelSpace([0,30],[0,30],[0,30],0.75)
-    print(np.shape(voxel.voxel))
-
+    xlim = [0.3,0]
+    ylim = [0.3,0]
+    zlim = [0.3,0]
+    voxel_size = [0.0075,0.0075,0.0075]
+    K = np.array([[891.318115234375, 0.0, 628.9073486328125],
+                  [0.0, 891.318115234375, 362.3601989746094],
+                  [0.0, 0.0, 1.0]], dtype=np.float32)
+    
+    voxel = VoxelSpace(xlim,ylim,zlim,voxel_size,K)
+    
+    image = cv2.imread("seg_image/view1.png",0)
+    extrinsic = np.array([[0.0, 1.0, 0.0, -0.15], 
+                          [0.8660254, 0.0, -0.5, -0.12990381], 
+                          [-0.5, 0.0, -0.8660254, 0.675]], dtype=np.float32)
+    
+    voxel.sfs(image,extrinsic)
 
